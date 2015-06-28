01aa9969fd06970a483bc05aa8e447ee9c329abf
hotpepper <heyseth@hotmail.com>
2015-06-28 14:21:37 -0400

    passes first values to html with render template

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -6,7 +6,9 @@ app= Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+	return render_template('index.html',
+						   title='Title passed from view template',
+						   text='text passed from view')
 
 if __name__=='__main__':
 	app.run(debug=True)
