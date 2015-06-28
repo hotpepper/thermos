97c58b8503b9b0f7c31786ba656b88f67f0d8f54
hotpepper <heyseth@hotmail.com>
2015-06-28 14:29:18 -0400

    pass object to template

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -3,12 +3,22 @@ from flask import Flask, render_template, url_for
 
 app= Flask(__name__)
 
+class User:
+	def __init__(self, firstname, lastername):
+		self.firstname = firstname
+		self.lastname = lastername
+	def initials(self):
+		return "{}. {}.".format(self.firstname[0], self.lastname[0])
+
+	def __str__(self):
+		return self.firstname+" "+self.lastname
+
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html',
 						   title='Title passed from view template',
-						   text='text passed from view')
+						   user=User("Seth","Hotpepper"))
 
 if __name__=='__main__':
 	app.run(debug=True)
